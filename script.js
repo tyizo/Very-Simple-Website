4316cb6ee83372d5930036428dfa43617d54a967
Tyizo <69811071+tyizo@users.noreply.github.com>
2021-01-15 14:41:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function myAlert(){
     alert('Soon , it will be better <3')
 };
+
+// For Disable Right Click
+document.onmouseclick = disableclick;
+stats = 'No , You Can Do Inspect';
+function disableclick(event){
+         if(event.button == 2){
+            alert(stats);
+            return false;
+         }; 
+};
